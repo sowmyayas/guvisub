96f1833cacd04a947c3633c71589e8b96da42409
sowmyayas <sowmyayas@gmail.com>
2016-06-08 10:19:28 +0530

    Create weekend.py

---
+++ weekend.py
@@ -0,0 +1,9 @@
+def main():
+    s1=input("string 1")
+    if((s1=="monday")or(s1=="tuesday")or(s1=="wednesday")or(s1=="thursday")or(s1=="friday")):
+        print("\ntrue")
+    elif((s1=="sunday")or(s1=="saturday")):
+        print("\nfalse")
+    else:
+        print("\ninvalid")
+main()        
