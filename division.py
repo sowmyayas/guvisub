0c254f0b19bd4846455353fa9810d39590e4141f
sowmyayas <sowmyayas@gmail.com>
2016-06-08 21:48:34 +0530

    Create division.py

---
+++ division.py
@@ -0,0 +1,19 @@
+
+def main():
+
+     a=int(input("a"))
+     b=int(input("b"))
+     r=0 
+     c=0
+    while(a>0):
+    
+        r=a-b 
+        a=r
+        c=c+1
+    
+    print("ans",c)
+    
+main()
+
+
+
