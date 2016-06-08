afa62dc8fbb3dc9f4adc5f7fc62d5e54df0157e4
sowmyayas <sowmyayas@gmail.com>
2016-06-08 11:59:16 +0530

    Create rerverseandremovevowels.py

---
+++ rerverseandremovevowels.py
@@ -0,0 +1,14 @@
+def main():
+	
+    s="sowmiyadevi"
+    s1=s[::-1]
+    print(s1)
+    s2=s1.replace('a','')
+    s3=s2.replace('i','')
+    s4=s3.replace('e','')
+    s5=s4.replace('o','')  
+    s6=s5.replace('u','')
+    
+    print(s6)
+main()
+ 
