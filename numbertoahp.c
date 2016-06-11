24efd3bb874e6b8cd2632bd15b98b13315d6bacb
sowmyayas <sowmyayas@gmail.com>
2016-06-11 20:50:54 +0530

    Create numbertoahp.c

---
+++ numbertoahp.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include<string.h>
+
+int main() {
+	char ahp[27]="_abcdefghijklmnopqrstuvwxyz";
+	int l;
+	l=strlen(ahp);
+	int number;
+	printf("%d",&number);
+	int i=0;
+    while(number>0)
+    {
+        sam[i]=nmber%10;
+        number=number/10;
+        i++;
+    }
+	return 0;
+}
+	
+	
+	
+	
+	
+	
