267441c2a31a773866a4869b3d28a8edbc42506b
sowmyayas <sowmyayas@gmail.com>
2016-06-11 19:25:19 +0530

    Create posornegative.py

---
+++ posornegative.py
@@ -0,0 +1,12 @@
+# your code goes here
+#python code goes here
+#python version :3 
+def main():
+    n=int(input("n"))
+    if(n==0):
+        print("\nzero")
+    elif(n>0):
+        print("\npositive")
+    elif(n<0):
+        print("\nnegavitive")
+main()        
